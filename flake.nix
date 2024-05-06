e7cafacf557f9872dea6c004c80d4fa962729076
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-05-06 21:40:16 +0200

    move to my external flake

--- flake.nix
+++ flake.nix
@@ -4,11 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOs/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nand2tetris = {
+      url = "github:0x5a4/nand2tetris-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
   outputs = {
     self,
     flake-utils,
+    nand2tetris,
     nixpkgs,
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -29,50 +35,6 @@
             cd $@
             ${pkgs.zip}/bin/zip -q9r "$OLDPWD/wienstroer$@.zip" .
           '';
-        
-          nand2tetristools = pkgs.stdenv.mkDerivation {
-            name = "nand2tetristools";
-            src = builtins.fetchTarball {
-              url = "https://drive.google.com/uc?export=download&id=1xZzcMIUETv3u3sdpM_oTJSTetpVee3KZ";
-              sha256 = "sha256:0n31p1kn4by3wmf1wqf40q7wnywxr8b1nxi49y9d08nniiricc3s";
-            };
-
-            installPhase = ''
-              mkdir -p $out/bin
-              cp -r ./tools/* $out/bin
-              chmod a+x $out/bin/*.sh
-            '';
-
-            postInstallPhase = ''
-            '';
-          };
-
-          hardwaresim = pkgs.symlinkJoin {
-            name = "hardwaresim";
-            paths = [nand2tetristools] ++ (with pkgs; [
-              openjdk 
-            ]);
-            buildInputs = [pkgs.makeWrapper];
-            postBuild = "wrapProgram $out/bin/HardwareSimulator.sh --prefix PATH : $out/lib/openjdk/bin";
-          };
-          
-          cpusim = pkgs.symlinkJoin {
-            name = "hardwaresim";
-            paths = [nand2tetristools] ++ (with pkgs; [
-              openjdk 
-            ]);
-            buildInputs = [pkgs.makeWrapper];
-            postBuild = "wrapProgram $out/bin/CPUEmulator.sh --prefix PATH : $out/lib/openjdk/bin";
-          };
-          
-          n2ttas = pkgs.symlinkJoin {
-            name = "hardwaresim";
-            paths = [nand2tetristools] ++ (with pkgs; [
-              openjdk 
-            ]);
-            buildInputs = [pkgs.makeWrapper];
-            postBuild = "wrapProgram $out/bin/Assembler.sh --prefix PATH : $out/lib/openjdk/bin";
-          };
         };
 
         apps = {
@@ -80,20 +42,11 @@
             drv = packages.submit;
           };
           
-          hardwaresim = flake-utils.lib.mkApp {
-            drv = packages.hardwaresim;
-            exePath = "/bin/HardwareSimulator.sh";
-          };
-          
-          cpusim = flake-utils.lib.mkApp {
-            drv = packages.cpusim;
-            exePath = "/bin/CPUEmulator.sh";
-          };
-          
-          n2ttas = flake-utils.lib.mkApp {
-            drv = packages.n2ttas;
-            exePath = "/bin/Assembler.sh";
-          };
+          hardwaresim = nand2tetris.apps.${system}.hardwaresimulator;
+          cpusim = nand2tetris.apps.${system}.cpusimulator;
+          nandasm = nand2tetris.apps.${system}.assembler;
+          nandvmemu = nand2tetris.apps.${system}.vmemulator;
+          nandcompiler = nand2tetris.apps.${system}.jackcompiler;
         };
       }
     );
