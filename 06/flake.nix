f268b6c8bc6cac308c7823003dd939f13884b7ec
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-05-07 17:39:00 +0200

    update assembler flake

--- 06/flake.nix
+++ 06/flake.nix
@@ -2,75 +2,55 @@
   description = "Zig project flake";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     zig2nix.url = "github:Cloudef/zig2nix";
+    nand2tetris.url = "github:0x5a4/nand2tetris-flake";
   };
 
-  outputs = { zig2nix, ... }: let
-    flake-utils = zig2nix.inputs.flake-utils;
-  in (flake-utils.lib.eachDefaultSystem (system: let
-      # Zig flake helper
-      # Check the flake.nix in zig2nix project for more options:
-      # <https://github.com/Cloudef/zig2nix/blob/master/flake.nix>
+  outputs = {
+    zig2nix,
+    flake-utils,
+    nand2tetris,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       env = zig2nix.outputs.zig-env.${system} {};
       system-triple = env.lib.zigTripleFromString system;
-    in with builtins; with env.lib; with env.pkgs.lib; rec {
-      # nix build .#target.{zig-target}
-      # e.g. nix build .#target.x86_64-linux-gnu
-      packages.target = genAttrs allTargetTriples (target: env.packageForTarget target ({
-        src = cleanSource ./.;
+    in
+      with builtins;
+      with env.lib;
+      with env.pkgs.lib; rec {
+        # nix build .#target.{zig-target}
+        # e.g. nix build .#target.x86_64-linux-gnu
+        packages.target = genAttrs allTargetTriples (target:
+          env.packageForTarget target {
+            src = cleanSource ./.;
 
-        nativeBuildInputs = with env.pkgs; [];
-        buildInputs = with env.pkgsForTarget target; [];
+            nativeBuildInputs = with env.pkgs; [];
+            buildInputs = with env.pkgsForTarget target; [];
 
-        # Smaller binaries and avoids shipping glibc.
-        zigPreferMusl = true;
+            # Smaller binaries and avoids shipping glibc.
+            # Wont be usable from nix tho
+            zigPreferMusl = true;
+            zigDisableWrap = true;
+          });
 
-        # This disables LD_LIBRARY_PATH mangling, binary patching etc...
-        # The package won't be usable inside nix.
-        zigDisableWrap = true;
-      } // optionalAttrs (!pathExists ./build.zig.zon) {
-        pname = "hackas";
-        version = "0.1.0";
-      }));
+        # nix build .
+        packages.default = packages.target.${system-triple}.override {
+          # Prefer nix friendly settings.
+          zigPreferMusl = false;
+          zigDisableWrap = false;
+        };
 
-      # nix build .
-      packages.default = packages.target.${system-triple}.override {
-        # Prefer nix friendly settings.
-        zigPreferMusl = false;
-        zigDisableWrap = false;
-      };
+        # nix run .
+        apps.default = env.app [] "zig build run -- \"$@\"";
 
-      # For bundling with nix bundle for running outside of nix
-      # example: https://github.com/ralismark/nix-appimage
-      apps.bundle.target = genAttrs allTargetTriples (target: let
-        pkg = packages.target.${target};
-      in {
-        type = "app";
-        program = "${pkg}/bin/default";
+        # nix run .#build
+        apps.build = env.app [] "zig build \"$@\"";
+
+        # nix run .#test
+        apps.test = env.app [] "zig build test -- \"$@\"";
+
+        apps.nandasm = nand2tetris.apps.${system}.assembler;
       });
-
-      # default bundle
-      apps.bundle.default = apps.bundle.target.${system-triple};
-
-      # nix run .
-      apps.default = env.app [] "zig build run -- \"$@\"";
-
-      # nix run .#build
-      apps.build = env.app [] "zig build \"$@\"";
-
-      # nix run .#test
-      apps.test = env.app [] "zig build test -- \"$@\"";
-
-      # nix run .#zon2json
-      apps.zon2json = env.app [env.zon2json] "zon2json \"$@\"";
-
-      # nix run .#zon2json-lock
-      apps.zon2json-lock = env.app [env.zon2json-lock] "zon2json-lock \"$@\"";
-
-      # nix run .#zon2nix
-      apps.zon2nix = env.app [env.zon2nix] "zon2nix \"$@\"";
-
-      # nix develop
-      devShells.default = env.mkShell {};
-    }));
 }
