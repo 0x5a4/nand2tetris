62eee20981f40b7ee7a9fcadf1f19ea05c5c9fff
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-06-07 15:20:49 +0200

    project 09 checkpoint

---
+++ 09/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "hackvmc";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixpkgs-unstable";
+    nand2tetris.url = "github:0x5a4/nand2tetris-flake";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    nand2tetris,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      defaultPackage = pkgs.stdenv.mkDerivation {
+        name = "diggy-diggy-hole";
+        src = ./.;
+
+        nativeBuildInputs = [
+          nand2tetris.defaultPackage.${system}
+        ];
+
+        buildPhase = ''
+          JackCompiler.sh  
+        '';
+
+        installPhase = ''
+          cat *.vm | tee $out
+        '';
+      };
+    
+      apps.vmemu = nand2tetris.apps.${system}.vmemulator;
+      apps.jackc = nand2tetris.apps.${system}.jackcompiler;
+    });
+}
